11460e3e01ddc82975584c1fbebb2f079d80bc2b
Muhammad_Mustafa <mmus33787@gmail.com>
2026-01-12 16:04:22 +0500

    grid view model task done

---
+++ lib/models/gridviewmodel.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Gridviewmodel{
+  final String title;
+  final IconData icon;
+
+  Gridviewmodel(
+  {
+    required this.title,
+    required this.icon,
+}
+      );
+}
